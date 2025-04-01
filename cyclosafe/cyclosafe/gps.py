b7d724941e6cf562ddb0e516ae8050f9f1de5390
Envently <pirard.nicolas@hotmail.fr>
2025-04-01 18:09:41 +0200

    implementing gps interface and improving gps informations

--- cyclosafe/cyclosafe/gps.py
+++ cyclosafe/cyclosafe/gps.py
@@ -1,8 +1,9 @@
 import rclpy
 from rclpy.node import Node
 from rclpy.executors import ExternalShutdownException
-from sensor_msgs.msg import NavSatFix
+from cyclosafe_interfaces.msg import NavSatInfo
 from cyclosafe.src.ASerialSensor import ASerialPublisher
+from sensor_msgs.msg import NavSatStatus
 
 """
 $GNGLL,4850.45401,N,00235.23105,E,111859.00,A,A*7E                              GPS+GLONASS, geographic position, latitude/longitude
@@ -17,6 +18,13 @@ $GPGSV,3,3,12,28,50,212,19,29,64,059,,31,57,255,19,49,34,177,*78                
 $GLGSV,3,1,10,66,00,281,,67,11,328,19,68,07,014,,74,13,133,*61                  GLONASS
 $GLGSV,3,2,10,75,65,125,30,76,48,319,30,77,04,316,,84,24,034,*69                |
 $GLGSV,3,3,10,85,76,068,14,86,38,200,32*65                                      |
+
+4, 5, 9, 16
+18, 20, 25, 26
+28, 29, 31, 49
+66, 67, 68, 74
+75, 76, 77, 84
+85, 86
 
 GLL => geographic position, latitude/longitude
 RMC => recommended minimum specific GPS data
@@ -42,15 +50,22 @@ Ex:
 class GPSPublisher(ASerialPublisher):
 	
 	def __init__(self):
-		super().__init__('gps', NavSatFix, 'gps', '/dev/ttyACM0', 115200)
+		super().__init__('gps', NavSatInfo, 'gps', '/dev/ttyACM0', 115200)
 		self.latitude = None
 		self.longitude = None
-		self.hdop = 5
+		self.hdop = None
+		self.pdop = None
+		self.ground_speed = None # km/h
+		self.active_sat = 0
+		self.altitude = None
+		self.status = NavSatStatus(status=NavSatStatus.STATUS_UNKNOWN)
 
 	def parse(self) -> bool:
 		"""Update stored values from GPS input. Return true if one or more value were updated"""
 		update: bool = False
 		lines = self.buffer.split(b'\n')
+		update = False
+		current_active = None
 		for line in lines:
 			words = line.decode("utf-8").split(',')
 			message_type = words[0]
@@ -60,22 +75,54 @@ class GPSPublisher(ASerialPublisher):
 				update = True
 			elif (message_type == "$GNGSA"):
 				self.hdop = float(words[16]) if words[16] != '' else float('NaN')
+				self.pdop = float(words[15]) if words[15] != '' else float('NaN')
+				if current_active != None:
+					current_active += sum(1 if word != "" else 0 for word in words[3:14])
 				update = True
+			elif (message_type == "$GNGGA"):
+				self.altitude = float(words[9]) if words[9] != '' else float('NaN')
+				update = True
+			elif (message_type == "$GNRMC"):
+				current_active = 0
+				self.status.service = 0
+				self.ground_speed = (float(words[7]) * 1.852) if words[7] != '' else float('NaN')
+				update = True
+			elif (message_type == "$GNVTG"):
+				self.ground_speed = float(words[7]) if words[7] != '' else float('NaN')
+				update = True
+			elif (message_type[:3] == "$GP"):
+				self.status.service |= NavSatStatus.SERVICE_GPS
+			elif (message_type[:3] == "$GL"):
+				self.status.service |= NavSatStatus.SERVICE_GLONASS
+			elif (message_type[:3] == "$GA"):
+				self.status.service |= NavSatStatus.SERVICE_GALILEO
 		if len(lines) > 1:
 			self.buffer = b'\n'.join(lines[:-1])
+		if current_active != None:
+			self.active_sat = current_active
+		if self.longitude != float('NaN') and self.latitude != float ('NaN'):
+			self.status.status = NavSatStatus.STATUS_FIX
+		else: self.status.status = NavSatStatus.STATUS_NO_FIX
 		return update
 	
-	def publish(self, data: NavSatFix):
+	def publish(self, data: NavSatInfo):
 		"""
 			TODO: Use timestamp sent by device instead of current
 		"""
-		msg = NavSatFix(latitude=self.latitude, longitude = self.longitude)
+		msg = NavSatInfo(
+			latitude=self.latitude, longitude = self.longitude, altitude = self.altitude,
+			hdop = self.hdop, pdop = self.pdop,
+			actives_sat = self.active_sat, status = self.status
+		)
 		msg.header.stamp = self.get_clock().now().to_msg()
-		msg.position_covariance[0] = self.hdop ** 2
-		msg.position_covariance[4] = self.hdop ** 2
-		msg.position_covariance[8] = (2 * self.hdop) ** 2
-		msg.position_covariance_type = NavSatFix.COVARIANCE_TYPE_APPROXIMATED
-		self.get_logger().debug(f"lat={msg.latitude}, lon={msg.longitude}, hdop={self.hdop}")
+		self.get_logger().debug(
+			f"lat={msg.latitude},\
+lon={msg.longitude},\
+alt={self.altitude},\
+speed={self.ground_speed},\
+actives={self.active_sat},\
+hdop={self.hdop},\
+pdop={self.pdop}")
 		self.pub.publish(msg)
 
 def main(args=None):
