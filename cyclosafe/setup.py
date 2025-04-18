1bccedbfcea39c8109ab142396f292483ba8f529
Envently <pirard.nicolas@hotmail.fr>
2025-04-18 13:37:52 +0200

    rm useless files and added COLCON_IGNORE to cyclosafe_lidar

--- cyclosafe/setup.py
+++ cyclosafe/setup.py
@@ -16,7 +16,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-		('share/' + package_name, ['launch/cyclosafe.launch.py', 'launch/test_config.launch.py', 'launch/viewer.launch.py']),
+		('share/' + package_name, ['launch/cyclosafe.launch.py', 'launch/viewer.launch.py']),
         ('share/' + package_name, ['launch/view.rviz', ]),
 		('share/' + package_name, ['scripts/gpio.py', 'scripts/gpio.sh', 'scripts/gps_time.sh']),
     ],
