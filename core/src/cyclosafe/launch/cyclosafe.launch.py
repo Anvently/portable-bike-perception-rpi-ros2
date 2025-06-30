7170980603c0feaf6e7c6c66f8552654a756113a
Envently <pirard.nicolas@hotmail.fr>
2025-06-30 15:10:12 +0200

    no more recordings when low storage is detected

--- core/src/cyclosafe/launch/cyclosafe.launch.py
+++ core/src/cyclosafe/launch/cyclosafe.launch.py
@@ -4,7 +4,7 @@ from launch.actions import DeclareLaunchArgument, OpaqueFunction, SetEnvironment
 from launch_ros.actions import Node
 from launch.substitutions import TextSubstitution, LaunchConfiguration
 import time
-import os, sys
+import os, sys, psutil
 import importlib.util
 from typing import List
 
@@ -13,6 +13,7 @@ launch_dir = os.path.join(package_dir, 'launch')
 sys.path.insert(0, launch_dir)
 from cyclosafe_config import Sensor, SensorTypeEnum
 
+LOW_STORAGE_TRESHOLD = int(os.getenv("LOW_STORAGE_TRESHOLD", "512"))
 
 def str2bool(v):
     return v.lower() in ("yes", "true", "t", "1")
@@ -35,6 +36,11 @@ launch_args = [
     DeclareLaunchArgument('record', default_value=TextSubstitution(text="false"), description="Capture every topic in a bag"),
     DeclareLaunchArgument('config', default_value=TextSubstitution(text=""), description="Optional path to a custom config file containing sensors_list"),
 ]
+
+def check_available_storage() -> bool:
+    if psutil.disk_usage("/").free / 1024 / 1024 < LOW_STORAGE_TRESHOLD:
+        return False
+    return True
 
 def setup_directory(parent_dir: str, time_start: float) -> str:
     if not parent_dir:
@@ -76,14 +82,16 @@ def launch_setup(context):
 
     ld = []
     if record:
-        path = setup_directory(parent_dir, time_start)
-        ld.extend([SetEnvironmentVariable(name='ROS_LOG_DIR', value=os.path.join(path, "logs"))])
-        if record:
+        if check_available_storage() == False:
+            print("Warning : low storage detected, no record will be made.")
+        else:
+            path = setup_directory(parent_dir, time_start)
+            ld.extend([SetEnvironmentVariable(name='ROS_LOG_DIR', value=os.path.join(path, "logs"))])
             ld.extend([
-                ExecuteProcess(
-                    cmd=['ros2', 'bag', 'record', '-a', '-b', '50000000', '--compression-mode', 'file', '--compression-format', 'zstd', '-o', os.path.join(path, "bag")],
-                    output='screen'
-                )
+                    ExecuteProcess(
+                        cmd=['ros2', 'bag', 'record', '-a', '-b', '50000000', '--compression-mode', 'file', '--compression-format', 'zstd', '-o', os.path.join(path, "bag")],
+                        output='screen'
+                    )
             ])
     for sensor in sensors_list:
         if sensor.enable == False or sensor.port == None:
